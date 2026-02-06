a0b8b08b8875969218e161b3de4b267528f31ef6
Varun Kushwah <varun.kush3@gmail.com>
2026-02-07 00:17:53 +0530

    feat: refactor App component and add routing
    - Removed unused imports and state management from App.tsx
    - Updated App component to use a new Button component and added a welcome message
    - Implemented routing using react-router with routes for About, Services, Login, and Signup pages
    
    chore: remove unused assets and update styles
    
    - Deleted react.svg asset as it was no longer needed
    - Refactored index.css to integrate Tailwind CSS and custom styles
    - Added custom CSS variables for theming and improved dark mode support
    
    feat: enhance TypeScript configuration
    
    - Updated tsconfig.app.json and tsconfig.json to include path aliases for easier imports
    
    chore: update Vite configuration
    
    - Added path aliasing in vite.config.ts for cleaner imports
    - Integrated Tailwind CSS plugin into Vite configuration
    
    feat: create reusable UI components
    
    - Added Button, Calendar, Card, Dialog, and Label components with utility functions
    - Implemented utility function for class name merging
    
    feat: add new pages
    
    - Created About, Login, Services, and Signup pages with basic structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Services from './pages/Services.tsx'
+import Signup from './pages/Signup.tsx'
+import About from './pages/About.tsx'
+import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  </BrowserRouter>
 )
